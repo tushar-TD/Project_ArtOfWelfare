886cdb03e2fef9bf37f53a07040979d40c2b6f7b
Tushar <dhanorkartushar2@gmail.com>
2023-08-17 22:53:58 +0530

    .

---
+++ AFW REACT/art/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Navbar from './Components/Navbar';
+import UserForm from './Components/UserForm';
+
+function App() {
+  return (
+    <div >
+      <header >
+        <Navbar />
+
+      </header>
+    </div>
+  );
+}
+
+export default App;

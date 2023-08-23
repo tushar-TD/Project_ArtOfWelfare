831ccbdb7701c8df3ac877fc1c2b5d41aaedb269
Tushar <dhanorkartushar2@gmail.com>
2023-08-24 02:00:19 +0530

    cart

--- AFW REACT/art/src/App.js
+++ AFW REACT/art/src/App.js
@@ -2,24 +2,27 @@ import './App.css';
 import 'bootstrap'
 import { Link, Route, Routes } from 'react-router-dom';
 import LoginForm from './component/LoginComp';
-import AdminHome from './component/AdminHome';
+
 import NgoHome from './component/NgoHome';
 import CustomerHome from './component/CustomerHome';
-import ArtistHome from './component/ArtistHome';
+
 import { useSelector } from 'react-redux';
 import LogoutComp from './component/LogoutComp';
 import ForgotPassword from './component/ForgotPassword';
 import CustomerReg from './component/CustomerReg';
 import NgoReg from './component/NgoReg';
 import ArtGallery from './component/ArtGallery';
+import Navcart from './component/Cart/Navcart';
 
 function App() {
   //referering initial state of logged
   const mystate = useSelector((state) => state.logged);
   return (
+
     <div>
+
       <div style={{ display: mystate.loggedIn ? "none" : "block" }}>
-        <nav className='navbar navbar-expand-sm bg-light mb-3'>
+        {/* <nav className='navbar navbar-expand-sm bg-light mb-3'>
           <div className='container-fluid'>
             <div className='collapse navbar-collapse'>
               <ul className='navbar-nav mr-auto'>
@@ -44,22 +47,24 @@ function App() {
               </ul>
             </div>
           </div>
-        </nav>
-        <ArtGallery />
+        </nav> */}
+
+        {/* <ArtGallery /> */}
+        <Navcart />
       </div>
 
 
       <Routes>
         <Route path='/login' element={<LoginForm></LoginForm>}></Route>
-        <Route path='/admin_home' element={<AdminHome></AdminHome>}></Route>
+        { }
         <Route path='/ngo_home' element={<NgoHome></NgoHome>}></Route>
         <Route path='/customer_home' element={<CustomerHome></CustomerHome>}></Route>
-        <Route path='/artist_home' element={<ArtistHome></ArtistHome>}></Route>
         <Route path='/logout' element={<LogoutComp></LogoutComp>}></Route>
         <Route path='/forgotpassword' element={<ForgotPassword></ForgotPassword>}></Route>
         <Route path='/reg_customer' element={<CustomerReg></CustomerReg>}></Route>
         <Route path='/NgoReg' element={<NgoReg></NgoReg>}></Route>
         <Route path='/ArtGallaery' element={<ArtGallery ></ArtGallery>} />
+
       </Routes>
 
       <h1 className="bg-primary text-center p-4">Welcome to Art for Welfare Platform</h1>
--- AFW REACT/art/src/component/ArtGallery.js
+++ AFW REACT/art/src/component/ArtGallery.js
@@ -6,25 +6,31 @@ const ArtGallery = () => {
 
     useEffect(() => {
         // Fetch art data from an API (replace with your API endpoint)
-        fetch('https://localhost:8080/unsoldarts')
+        fetch('http://localhost:8080/unsoldarts')
             .then(response => response.json())
-            .then(data => setArtList(data))
+            .then(data => {
+                setArtList(data);
+                console.log(data);
+            })
             .catch(error => console.error('Error fetching data:', error));
     }, []);
+
 
     return (
         <div className="art-gallery">
             {artList.map((artPiece) => (
-                <div key={artPiece.id} className="art-card">
-                    <img src={artPiece.image_url} alt={artPiece.title} />
+                <div key={artPiece.art_id} className="art-card">
+                    <img src={artPiece.image} alt={artPiece.title} />
                     <h3>{artPiece.art_name}</h3>
-                    <p>Artist: {artPiece.description}</p>
+                    <p>Description: {artPiece.description}</p>
                     <p>Artist: {artPiece.artist_name}</p>
                     <p>Price: {artPiece.price}</p>
-                    <button>Buy Now</button>
+                    <button className="btn btn-danger" >Buy Now</button>
                 </div>
-            ))}
-        </div>
+            ))
+            }
+
+        </div >
     );
 };
 
